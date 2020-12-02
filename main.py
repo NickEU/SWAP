a6971302dd3f9757eaedb14fe585381f0523aace
xstyle <kioto@inbox.lv>
2020-12-02 23:28:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+#x = y = z = 5
+#print(x + y + z)
+
+#a, b, c = 3, 20, 100
+#print(a + b + c)
+
+#SWAP-1
+"""
+a = 2
+b = 5
+print("a= " + str(a))
+print("b= " + str(b))
+print()
+tmp=a
+a=b
+b=tmp
+print("a= " + str(a))
+print("b= " + str(b))
+"""
+
+#SWAP-2
+"""
+a = 2
+b = 5
+tmp1, tmp2 = a, b
+a, b = tmp2, tmp1
+print(a)
+print(b)
+
+"""
+
+#SWAP-3 !!!!!
+a = 2
+b = 5
+print(a)
+print(b)
+print()
+a, b = b, a   #!!!!!!!!!!!
+print(a)
+print(b)
